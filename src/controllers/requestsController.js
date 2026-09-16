8bad6173fcecfe2dcb79f9457c7098f737b65865
Aleksandr Maslov <hi@mslv.space>
2026-09-16 21:17:00 +0300

    feat(requests): CRUD заявок и смена статуса по таблице переходов

---
+++ src/controllers/requestsController.js
@@ -0,0 +1,37 @@
+import { sendList, sendCreated } from "./respond.js";
+
+export function createRequestsController({ requestService }) {
+  return {
+    async list(req, res) {
+      sendList(res, await requestService.list(req.validated.query));
+    },
+
+    async listByEquipment(req, res) {
+      const { params, query } = req.validated;
+      sendList(res, await requestService.listByEquipment(params.id, query));
+    },
+
+    async getById(req, res) {
+      res.json({ data: await requestService.getById(req.validated.params.id) });
+    },
+
+    async create(req, res) {
+      sendCreated(req, res, await requestService.create(req.validated.body));
+    },
+
+    async update(req, res) {
+      const { params, body } = req.validated;
+      res.json({ data: await requestService.update(params.id, body) });
+    },
+
+    async changeStatus(req, res) {
+      const { params, body } = req.validated;
+      res.json({ data: await requestService.changeStatus(params.id, body.status) });
+    },
+
+    async remove(req, res) {
+      await requestService.remove(req.validated.params.id);
+      res.status(204).end();
+    },
+  };
+}
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import { createEquipmentService } from "./equipmentService.js";
+import { createRequestService } from "./requestService.js";
 
 export function createServices({ repositories }) {
   return {
     equipmentService: createEquipmentService(repositories),
+    requestService: createRequestService(repositories),
   };
 }
